d6a98202613779a44d1a0ded0804481b0aa1f7a8
Daniil <daniil@cab-wsn-0004393.sigma.sbrf.ru>
2020-09-18 12:33:17 +0300

    .

--- metrics.py
+++ metrics.py
@@ -102,9 +102,9 @@ def _global_stats_f1(results):
 
 def ner_token_f1(y_true, y_pred, print_results=False):
 
-    print('########################')
-    print('true: ', len(y_true))
-    print('pred: ', len(y_pred))
+    if len(y_true) != len(y_pred):
+        print('true: ', y_true)
+        print('pred: ', y_pred)
 
     y_true = list(chain(*y_true))
     y_pred = list(chain(*y_pred))
