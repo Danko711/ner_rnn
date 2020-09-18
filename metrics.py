fbdd40fb07f60bbb7a51cd9039b8a5b65786f36e
Daniil <daniil@cab-wsn-0004393.sigma.sbrf.ru>
2020-09-18 11:54:58 +0300

    .

--- metrics.py
+++ metrics.py
@@ -104,6 +104,10 @@ def ner_token_f1(y_true, y_pred, print_results=False):
     y_true = list(chain(*y_true))
     y_pred = list(chain(*y_pred))
 
+    print('########################')
+    print('true: ', len(y_true))
+    print('pred: ', len(y_pred))
+
     # Drop BIO or BIOES markup
     assert all(len(str(tag).split('-')) <= 2 for tag in y_true)
 
@@ -119,10 +123,6 @@ def ner_token_f1(y_true, y_pred, print_results=False):
     for tag, tag_ind in tags_dict.items():
         if tag == 'O':
             continue
-        print('##############################')
-        print('true: ', len(y_true_inds))
-        print('pred: ', len(y_pred_inds))
-        #print('tag: ', len(tag_ind))
         tp = np.sum((y_true_inds == tag_ind) & (y_pred_inds == tag_ind))
         fn = np.sum((y_true_inds == tag_ind) & (y_pred_inds != tag_ind))
         fp = np.sum((y_true_inds != tag_ind) & (y_pred_inds == tag_ind))
