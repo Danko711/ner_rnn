6d5cda9e213c23fd9cca353b459698b5bd63e7c6
Daniil <daniil@MacBook-Air-Daniil.local>
2020-09-23 00:42:25 +0300

    .

--- supervised_runner.py
+++ supervised_runner.py
@@ -59,8 +59,8 @@ callbacks = {
         grad_clip_params=None
     ),
     "criterion": dl.CriterionCallback(
-        input_key=['x', 'x_char', 'y'],
-        #output_key=('preds'),
+        input_key='y',
+        output_key=['x', 'x_char'],
         prefix='loss'
     ),
     "metric": dl.MetricCallback(
