9f4f46b0e527ba315ddb26493ac08731b8fe5724
Daniil <daniil@MacBook-Air-Daniil.local>
2020-09-22 22:48:25 +0300

    .

--- supervised_runner.py
+++ supervised_runner.py
@@ -60,7 +60,7 @@ callbacks = {
     ),
     "criterion": dl.CriterionCallback(
         input_key=['x', 'x_char', 'y'],
-        output_key=['pred']
+        output_key='pred'
     ),
     "metric": dl.MetricCallback(
         input_key='total_tags',
