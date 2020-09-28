99d6b7cc96ae11c42847b168495831f36120418b
Daniil <daniil@inca-0120102.sigma.sbrf.ru>
2020-09-28 13:14:50 +0300

    .

--- catalyst_custom_runner.py
+++ catalyst_custom_runner.py
@@ -135,5 +135,23 @@ runner.train(model=model,
              timeit=False,
              logdir='./checkpoints',
              scheduler=None,
-             callbacks=callbacks
+             callbacks={
+                 "optimizer": dl.OptimizerCallback(
+                     metric_key="loss",
+                     accumulation_steps=1,
+                     grad_clip_params=None
+                 ),
+                 "criterion": dl.CriterionCallback(
+                     input_key=['x', 'x_char', 'y'], #'mask': mask,
+                     output_key=[]
+                 ),
+                 "metric": dl.MetricCallback(
+                     input_key='total_tags',
+                     output_key='preds',
+                     prefix='F1_token',
+                     metric_fn=ner_token_f1
+
+
+                 )
+             }
              )
